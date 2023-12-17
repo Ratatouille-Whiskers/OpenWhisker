352f5fd6be083ebe64636f07d8530ac79f9dddf6
OmarFaris <omaar97@gmail.com>
2023-12-17 20:04:49 +0000

    Model.py 1st version: gives deflection at the tip

---
+++ OmarMathModel/Model.py
@@ -0,0 +1,9 @@
+import matplotlib
+import numpy as np
+
+# Define constants:
+
+E = 3500 # Young Modulus of PLA in MPa
+d = 2 # diameter in mm
+I = np.pi * d **4 / 64 # Area moment of inertia for a circle
+b = 180 # length of the whisker in mm
